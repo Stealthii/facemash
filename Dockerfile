4a44587f73d75bff6f3e3d08863c322e6ca3aa54
Daniel Porter <dan.porter@rehabstudio.com>
2015-08-05 13:46:02 +0100

    Add basic faceswap logic

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,18 @@ FROM debian:jessie
 
 # install dependencies from apt
 RUN apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y python python-dev python-pip nginx sqlite3 supervisor uwsgi uwsgi-plugin-python && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y cmake curl libboost-python-dev python python-dev python-opencv python-pil python-pip nginx sqlite3 supervisor uwsgi uwsgi-plugin-python && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
+
+# download, unpack, compile and install dlib
+RUN cd /tmp && curl http://dlib.net/files/dlib-18.16.tar.bz2 | tar xj && \
+    cd /tmp/dlib-18.16/python_examples && ./compile_dlib_python_module.bat && \
+    cp ./build/dlib.so /usr/local/lib/python2.7/dist-packages/ && \
+    rm -rf /tmp/* /var/tmp/*
+
+# download and unpack trained model from sourceforge
+RUN mkdir -p /models && curl http://netcologne.dl.sourceforge.net/project/dclib/dlib/v18.10/shape_predictor_68_face_landmarks.dat.bz2 | bunzip2 -c > /models/shape_predictor_68_face_landmarks.dat
 
 # setup all the configfiles
 ADD ./supervisord.conf /etc/supervisord.conf
--- app/wsgi.py
+++ app/wsgi.py
@@ -1,14 +1,66 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-application = Flask(__name__)
+from cStringIO import StringIO
 
-@application.route('/', methods=['GET'])
+import faceswap
+from flask import Flask
+from flask import request
+from flask import send_file
+import requests
+import tempfile
+
+
+app = Flask(__name__, static_folder='/srv/static')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello world!'
+    if request.method == 'POST':
+        # Get the files
+        if request.form.has_key('head_url'):
+            head_image = get_image_from_url(request.form.get('head_url'))
+        elif request.files.has_key('head_upload'):
+            head_image = request.files.get('head_upload')
+        else:
+            complain("No head image")
+
+        if request.form.has_key('face_url'):
+            face_image = get_image_from_url(request.form.get('face_url'))
+        elif request.files.has_key('face_upload'):
+            face_image = request.files.get('face_upload')
+        else:
+            complain("No face image")
+
+        # Do the merge
+        merged_image = faceswap.merge_images(head_image, face_image)
+        return send_file(
+            StringIO(merged_image),
+            attachment_filename="test.jpg",
+            as_attachment=False
+        )
+
+    else:
+        # Show merge form
+        return app.send_static_file('index.html')
+
+
+def get_image_from_url(url):
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        f = tempfile.TemporaryFile()
+        for chunk in r.iter_content(1024):
+            f.write(chunk)
+        f.seek(0)
+        return f
+    return None
+
+
+def complain(string):
+    return string
+
 
 def test():
-    application.run(debug=True)
+    app.run(debug=True)
 
 if __name__ == '__main__':
     test()
