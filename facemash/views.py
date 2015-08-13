6f41f6c8882022e4f463b5a268112d71ec375a1d
Daniel Porter <dan.porter@rehabstudio.com>
2015-08-13 15:34:20 +0100

    Template for returned file

--- facemash/views.py
+++ facemash/views.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from cStringIO import StringIO
-
-import faceswap
+from facemash import app
+from flask import render_template
 from flask import request
-from flask import send_file
+import faceswap
+import os
 import requests
 import tempfile
-
-
-from facemash import app
+import time
 
 
 @app.route('/upload', methods=['POST'])
@@ -29,17 +27,26 @@ def upload():
         merged_image = faceswap.merge_images(head_image, face_image)
     except:
         raise
-    return send_file(
-        StringIO(merged_image),
-        attachment_filename="test.jpg",
-        as_attachment=False
-    )
+
+    filename = time.strftime("%Y%m%d-%H%M%S") + ".jpg"
+    filepath = os.path.join("/srv/media", filename)
+    servepath = os.path.join("/media", filename)
+    with open(filepath, 'wb') as f:
+        f.write(merged_image)
+
+    return render_template('image.html', mashed_image=servepath)
 
 
 @app.route('/', methods=['GET'])
 def index():
     # Show merge form
     return app.send_static_file('index.html')
+
+
+@app.route('/simple', methods=['GET'])
+def simple_index():
+    # Show merge form
+    return app.send_static_file('simple_form.html')
 
 
 def get_image_from_url(url):
